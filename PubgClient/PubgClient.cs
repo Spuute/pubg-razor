ad5410a525e2fd2f1452bf74c94dc57fec36ba76
Spuute <spuute@gmail.com>
2022-02-20 10:37:36 +0100

    update

--- PubgClient/PubgClient.cs
+++ PubgClient/PubgClient.cs
@@ -20,7 +20,7 @@ namespace pubgstats.PubgClient
             var client = new RestClient("https://api.pubg.com/shards/steam/seasons/division.bro.official.pc-2018-15/gameMode/solo-fpp/");
             client.Authenticator = new JwtAuthenticator("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiMWU2ODk0MC00OTU0LTAxMzktOWI4My0wMTVjZDc2MmZiYTciLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjEyNDcwMTc4LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymctc3RhdHMtZmluIn0.gCOOQtprWoYa__2RIU3mHMIf4qplt9jojZjQJjdtgjM");
 
-            var request = new RestRequest("players?filter[playerIds]=account.c0e530e9b7244b358def282782f893af&filter[gamepad]=false");
+            var request = new RestRequest("players?filter[playerIds]=account.c0e530e9b7244b358def282782f893af&filter[gamepad]=false", Method.GET);
             request.AddHeader("Accept", "application/json");
             var response = await client.ExecuteAsync<PlayerSeasonStats>(request);
 
